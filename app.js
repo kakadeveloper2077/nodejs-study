57c95ac05fa483efb01364346886274e205d84b4
Kayky Antonio <kaykyantonio010@gmail.com>
2026-05-05 11:27:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = 'Michael Jackson';
+let age = '29';
+function show_name(name,age){
+   return console.log('the ' + name + ' has ' + age + ' years' + '.');
+} 
+show_name(name, age);
---
+++ calculator.js
@@ -0,0 +1,9 @@
+var AddFunc = require('./add');
+var MinusFunc = require('./minus'); 
+var DivideFunc = require('./divide');
+var MultiplyFunc = require('./multiply');
+
+console.log(AddFunc(10,25))
+console.log(MinusFunc(10123123,2943256329456785))
+console.log(DivideFunc(10,25))
+console.log(MultiplyFunc(10,25))
